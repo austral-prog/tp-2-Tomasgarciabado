aece7929cc628e2111f5101be3f2ca09d2ac86fe
Tomasgarciabado <tgarciabado@mail.austral.edu.ar>
2025-04-04 12:10:35 -0300

    Update change.py

--- change.py
+++ change.py
@@ -8,7 +8,6 @@ def change():
     vuelto = dinero_recibido - gasto
     pesos = int(vuelto)
     centavos = int(round((vuelto - pesos) * 100))
-
     print("\nVuelto\n")
     print(f"Pesos:\n{pesos}")
     print(f"Centavos:\n{centavos}")
